3b8c382c9ab57290f365607c18fcce72f7a196fa
TsvetelinIliev <bho981@gmail.com>
2026-07-06 17:57:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import RootNavigator from './navigation/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 
