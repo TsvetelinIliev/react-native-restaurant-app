7219804dc8561d0e2f14078f74c95b05c1e2a2b2
TsvetelinIliev <bho981@gmail.com>
2026-07-06 19:34:51 +0100

    Add feature items on home screen

--- src/navigation/HomeNavigator.jsx
+++ src/navigation/HomeNavigator.jsx
@@ -1,15 +1,23 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import HomeScreen from "../screens/HomeScreen";
 import CartScreen from "../screens/CartScreen";
+import { View } from "react-native";
+
 
 export default function HomeNavigator() {
 
-    const Stack = createNativeStackNavigator();
+    const Stack =createNativeStackNavigator();
 
     return (
-        <Stack.Navigator >
 
-            <Stack.Screen   name="Home" component={HomeScreen} />
+        <Stack.Navigator screenOptions={{ headerShown: false }}  >
+
+            
+
+           
+            <Stack.Screen  name="Home" component={HomeScreen} />
+            
+
             
 
         </Stack.Navigator>
