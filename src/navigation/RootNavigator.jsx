358791e1c45c1c027b5bdfea35e97d528fd66da8
TsvetelinIliev <bho981@gmail.com>
2026-07-06 17:55:01 +0100

    Extract info screen

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -2,22 +2,33 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeNavigator from "./HomeNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import CartScreen from "../screens/CartScreen";
+import InfoScreen from "../screens/InfoScreen";
+import HomeScreen from "../screens/HomeScreen";
 
 export default function RootNavigator() {
 
     const Tabs = createBottomTabNavigator();
     return (
 
-        
 
 
-            <Tabs.Navigator>
-                <Tabs.Screen  name="HomeTab"  component={HomeNavigator} />
-                <Tabs.Screen   name="Cart" component={CartScreen} />
-                <Tabs.Screen  name="Info" component={() => <Text>Info Screen</Text>}  />
-            </Tabs.Navigator>
 
+         <Tabs.Navigator >
        
+                    <Tabs.Screen
+                        name="HomeTab"
+                        component={HomeNavigator}
+                        // options={{
+                        //     title: "Home",
+                            
+                        //     headerShown: false
+                        // }}
+                    />
+            <Tabs.Screen name="Cart" component={CartScreen} />
+            <Tabs.Screen name="Info" component={InfoScreen} />
+        </Tabs.Navigator>
+
+
 
     );
 };
---
+++ src/screens/InfoScreen.jsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export default function InfoScreen() {
+
+    return (
+
+        <Text>Info Screen</Text>
+    )
+}
